ec1cc0b7fda22ea651e3c34170b4e2281004e228
antr0380 <70625831+antr0380@users.noreply.github.com>
2025-11-18 21:24:48 +0100

    Revert "DaisyUI and login page"
    This reverts commit 2c24c6c19b0998c213ced8c25e9deab55c106e8c.

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/login/page.tsx
-
-export default function MainPage() {
-	return (
-		<main className="min-h-screen flex items-center justify-center bg-base-200">
-			<button className = "btn">Testing</button>
-		</main>
-	);
-}
