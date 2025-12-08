4abbc0716b47aaa1f72ac02ec6de9b2f78afa60d
antr0380 <70625831+antr0380@users.noreply.github.com>
2025-12-08 19:32:03 +0100

    Use icon for edit schedule button on small screens

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -228,7 +228,18 @@ export default function AthleteSchedulePage() {
                 onClick={handleModifyWeek}
                 disabled={!activeWeekId}
               >
-                Redigera schema
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="h-4 w-4 text-white sm:hidden"
+                  aria-hidden
+                >
+                  <path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path>
+                </svg>
+                <span className="sr-only sm:not-sr-only sm:inline">
+                  Redigera schema
+                </span>
               </button>
             }
           />
